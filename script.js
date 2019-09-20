44625be06238d4c3da443aad442df87f00cb80f1
willypark22 <dpark032@ucr.edu>
2019-09-20 16:09:37 -0700

    got the timer to display and work correctly

--- script.js
+++ script.js
@@ -4,12 +4,26 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+var timer = [0, 0, 0, 0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+function leadingZero(time) {
+  if(time <= 9) {
+    time = "0" + time;
+  }
+  return time;
+}
 
 // Run a standard minute/second/hundredths timer:
+function runTimer() {
+  let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+  theTimer.innerHTML = currentTime;
+  timer[3]++;
 
+  timer[0] = Math.floor((timer[3]/100)/60);
+  timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+  timer[2] = Math.floor(timer[3] - timer[1] * 100) - (timer[0] * 6000);
+}
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
@@ -20,12 +34,18 @@ function spellCheck() {
 // Start the timer:
 function start() {
   let textEnteredLength = testArea.value.length;
+  if(textEnteredLength === 0) {
+    setInterval(runTimer, 10);
+  }
   console.log(textEnteredLength);
 }
 
 // Reset everything:
-
+function reset() {
+  console.log("Reset button has been pressed!");
+}
 
 // Event listeners for keyboard input and the reset button:
 testArea.addEventListener("keypress", start, false);
 testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
